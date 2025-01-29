dd1fe15aa9efa7d0625c82ea36828c668c57bf8c
Gunther Rademacher <grd@gmx.net>
2025-01-29 22:43:24 +0100

    update copyright notice

--- src/main/java/de/bottlecaps/markup/blitz/transform/Copy.java
+++ src/main/java/de/bottlecaps/markup/blitz/transform/Copy.java
@@ -1,4 +1,4 @@
-// Copyright (c) 2023-2024 Gunther Rademacher. Provided under the Apache 2 License.
+// Copyright (c) 2023-2025 Gunther Rademacher. Provided under the Apache 2 License.
 
 package de.bottlecaps.markup.blitz.transform;
 
